9f6280ee25f990e636e4c010aeb55b4e169279f8
josue.dosidiana <josue.dosidiana@gmail.com>
2024-08-23 18:09:32 +0100

    update:projecto implementado

--- DesafioControleFluxo/src/projecto/Contador.java
+++ DesafioControleFluxo/src/projecto/Contador.java
@@ -1,5 +1,37 @@
 package projecto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Contador {
 
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int primeiroNumero, segundoNumero;
+
+        System.out.print("Digite o primeiro parametro:");
+        primeiroNumero = scanner.nextInt();
+        System.out.print("Digite o segundo parametro:");
+        segundoNumero = scanner.nextInt();
+
+        try{
+            contador(primeiroNumero, segundoNumero);
+
+        }catch(ParametrosInvalidosException exception){
+            exception.printStackTrace();
+        }finally{
+            scanner.close();
+        }
+    }
+    public static void contador(int primeiroParametro, int segundoParametro)throws ParametrosInvalidosException{
+        
+        if(segundoParametro > primeiroParametro){
+            int contador = segundoParametro - primeiroParametro;
+            for( int i = 1; i <= contador; i++){
+                System.out.printf("\nImprimindo o número %d",i);
+            }
+        }else
+            throw new ParametrosInvalidosException();
+
+    }
 }
--- DesafioControleFluxo/src/projecto/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/projecto/ParametrosInvalidosException.java
@@ -1,5 +1,10 @@
 package projecto;
 
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception{
 
+    public ParametrosInvalidosException(){
+
+        System.out.print("Opps! Algo correu mal.");
+
+    }
 }
